773eec529cf2b5292ecd31451f9b7c571779356b
Asaia Palacios <asaia.palacios@gmail.com>
2016-03-23 17:13:20 -0700

    Executing regular expressions in Ruby script. Task 0 and 1

---
+++ use_a_regular_expression_against_a_problem_now_you_have_2_problems/0-letters_and_numbers.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/ruby
+
+puts (ARGV[0].scan(/[A-Za-z0-9]+/)).join # + means character needs to appear at least one time 
+
+
---
+++ use_a_regular_expression_against_a_problem_now_you_have_2_problems/2-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/ruby
+
+puts (ARGV[0].scan(/[\!A-Z]/)).join # inlcude join to join all characters into one line
